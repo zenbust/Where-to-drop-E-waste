5ca7a62fe1e0fe431374582689f5bf04845b8e83
Newwich <nontneww2008@gmail.com>
2024-11-04 11:00:22 +0700

    Add files via upload

--- WTDEW/WTDEW.js
+++ WTDEW/WTDEW.js
@@ -18,6 +18,38 @@ const infoButton = document.getElementById("infoButton");
         popupOverlay.classList.remove("show");
     });
 //
+const locations = [
+    "Triam Udon Suksa Patthanakarn, Nonthaburi",
+    "Central Plaza, Nonthaburi",
+    "Future Park Rangsit, Pathum Thani",
+    "Big C, Ratchaphruek",
+    "Home Pro, Nonthaburi",
+    "โรงเรียนเตรียมอุดมศึกษาพัฒนาการนนทบุรี",
+    "Central Plaza Wasgate"
+];
+
+function showSuggestions(value) {
+    const suggestionsContainer = document.getElementById('suggestions');
+    suggestionsContainer.innerHTML = ''; 
+
+    if (value.length === 0) return; 
+
+    const filteredSuggestions = locations.filter(item => 
+        item.toLowerCase().includes(value.toLowerCase())
+    );
+
+    filteredSuggestions.forEach(item => {
+        const suggestionItem = document.createElement('div');
+        suggestionItem.classList.add('suggestion');
+        suggestionItem.textContent = item;
+        suggestionItem.onclick = () => {
+            document.getElementById('Search').value = item; 
+            suggestionsContainer.innerHTML = ''; 
+        };
+        suggestionsContainer.appendChild(suggestionItem);
+    });
+}
+//
 const locationInfo = document.getElementById('location-info');
 
 const locationsData = [
