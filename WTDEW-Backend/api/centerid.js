a710e6a9fdc37abf808bf5a79cdd4054e5eeaf3d
zenbust <85044295+zenbust@users.noreply.github.com>
2024-11-10 17:03:02 +0700

    Add files via upload

--- WTDEW-Backend/api/centerid.js
+++ WTDEW-Backend/api/centerid.js
@@ -5,6 +5,9 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     const centerId = req.query.id;
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     try {
         const center = await getCenterById(centerId);
         if (!center) {
--- WTDEW-Backend/api/centers.js
+++ WTDEW-Backend/api/centers.js
@@ -1,6 +1,12 @@
 const { getCenters } = require('../db');
 
 module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     try {
         const centers = await getCenters();
         res.json(centers);
