9d2c8e269eaff089540f66b9b38fb5dd5e4e6a04
zenbust <85044295+zenbust@users.noreply.github.com>
2024-11-10 20:31:20 +0700

    Add files via upload

--- WTDEW-Backend/api/centerid.js
+++ WTDEW-Backend/api/centerid.js
@@ -1,26 +1,21 @@
 const { getCenterById } = require('../db');
 
-module.exports = async (req, res) => {
-    const allowCors = fn => async (req, res) => {
-        res.setHeader('Access-Control-Allow-Credentials', true)
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-        if (req.method === 'OPTIONS') {
-            res.status(200).end()
-            return
-        }
-        return await fn(req, res)
-        }
-        const handler = (req, res) => {
-        const d = new Date()
-        res.end(d.toString())
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
     }
-    module.exports = allowCors(handler)
+    return await fn(req, res);
+};
 
+const handler = async (req, res) => {
     const centerId = req.query.id;
     try {
         const center = await getCenterById(centerId);
@@ -33,3 +28,5 @@ module.exports = async (req, res) => {
         res.status(500).json({ error: "Failed to fetch center data" });
     }
 };
+
+module.exports = allowCors(handler);
