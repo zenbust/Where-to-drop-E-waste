368873f704a9f12edd813f56f5a33986bcdc28ec
zenbust <85044295+zenbust@users.noreply.github.com>
2024-11-10 16:13:44 +0700

    Add files via upload

--- WTDEW-Backend/db.js
+++ WTDEW-Backend/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { MongoClient, ObjectId } = require('mongodb');
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 async function connectDB() {
     if (!client.isConnected()) {
